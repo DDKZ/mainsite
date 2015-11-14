58f7a066c4ba33f1f10e82db05b9752be99e7edb
theworldbright <kent@kentshikama.com>
2015-11-14 00:51:28 -0800

    Fix URLs and templates

--- aspc/geonet/urls.py
+++ aspc/geonet/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
-from aspc.geonet.views import home, requestFriend, approveFriend, selfUpdate
+from aspc.geonet.views import home
 
 urlpatterns = [
     url(r'^$', home, name="geonet_home"),
-    url(r'^request/$', requestFriend, name='friend_request'),
-    url(r'^approve/(?P<geouser_id>[0-9]+)/$', approveFriend, name='friend_approve'),
-    url(r'^self-update/$', selfUpdate, name='self_update')
 ]
